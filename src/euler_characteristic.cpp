c63f3fb52adecf1ac1012eff0117eca8e7260912
errorbound <bound.for.error@gmail.com>
2017-01-22 17:54:15 -0500

    Added euler characteristic calculation

--- src/euler_characteristic.cpp
+++ src/euler_characteristic.cpp
@@ -1,8 +1,29 @@
 #include "euler_characteristic.h"
+#include "edges.h"
 
 int euler_characteristic(const Eigen::MatrixXi &F)
 {
   int Chi;
-  // ADD YOUR CODE HERE
+  int nfaces, nedges, nverts; 
+
+  nfaces = F.rows(); 
+
+  Eigen::MatrixXi E = edges(F);
+  nedges = E.rows(); 
+
+  // Still need to find the number of vertices; don't know how many edges go to each vertex
+  int maxv = 0; 
+  for (int f = 0; f < F.rows(); ++f) {
+    if (F(f, 0) > maxv)
+      maxv = F(f, 0); 
+    if (F(f, 1) > maxv) 
+      maxv = F(f, 1); 
+    if (F(f, 2) > maxv)
+      maxv = F(f, 2); 
+  }
+  nverts = maxv + 1; 
+
+  Chi = nverts - nedges + nfaces; 
+
   return Chi;
 }
